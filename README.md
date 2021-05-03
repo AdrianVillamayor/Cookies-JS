72fd66c5b4d809735b5a02ca30b6080f790f4358
Adrii <adrian.villamayor@gmail.com>
2021-05-03 12:48:45 +0200

    Update

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# cookie.js
+# cookie_alert.js
+## How to use
+#### 1. In the `head` of your document, include `cookiealert.css` **after Bootstrap**.
+```html
+<link rel="stylesheet" href="cookie_alert.css">
+```
+#### 2. Include the JavaScript after the html markup
+```html
+<script src="cookie_alert.js"></script>
+```
+
+### Accept event
+If you need to, you can listen for the `cookieAlertAccept` event to get notified when the user accepts the cookies.
+
+```js
+window.addEventListener("cookieAlertAccept", function() {
+    alert("cookies accepted")
+})
+```
---
+++ cookies.js
@@ -0,0 +1,203 @@
+(function (factory) {
+    // https://github.com/js-cookie/js-cookie
+    var registeredInModuleLoader;
+    if (typeof define === 'function' && define.amd) {
+        define(factory);
+        registeredInModuleLoader = true;
+    }
+    if (typeof exports === 'object') {
+        module.exports = factory();
+        registeredInModuleLoader = true;
+    }
+    if (!registeredInModuleLoader) {
+        var OldCookies = window.Cookies;
+        var api = window.Cookies = factory();
+        api.noConflict = function () {
+            window.Cookies = OldCookies;
+            return api;
+        };
+    }
+}(function () {
+    function extend() {
+        var i = 0;
+        var result = {};
+        for (; i < arguments.length; i++) {
+            var attributes = arguments[i];
+            for (var key in attributes) {
+                result[key] = attributes[key];
+            }
+        }
+        return result;
+    }
+
+    function decode(s) {
+        return s.replace(/(%[0-9A-Z]{2})+/g, decodeURIComponent);
+    }
+
+    function init(converter) {
+        function api() {}
+
+        function set(key, value, attributes) {
+            if (typeof document === 'undefined') {
+                return;
+            }
+
+            attributes = extend({
+                path: '/'
+            }, api.defaults, attributes);
+
+            if (typeof attributes.expires === 'number') {
+                attributes.expires = new Date(new Date() * 1 + attributes.expires * 864e+5);
+            }
+
+            // We're using "expires" because "max-age" is not supported by IE
+            attributes.expires = attributes.expires ? attributes.expires.toUTCString() : '';
+
+            try {
+                var result = JSON.stringify(value);
+                if (/^[\{\[]/.test(result)) {
+                    value = result;
+                }
+            } catch (e) {}
+
+            value = converter.write ?
+                converter.write(value, key) :
+                encodeURIComponent(String(value))
+                .replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
+
+            key = encodeURIComponent(String(key))
+                .replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent)
+                .replace(/[\(\)]/g, escape);
+
+            var stringifiedAttributes = '';
+            for (var attributeName in attributes) {
+                if (!attributes[attributeName]) {
+                    continue;
+                }
+                stringifiedAttributes += '; ' + attributeName;
+                if (attributes[attributeName] === true) {
+                    continue;
+                }
+
+                // Considers RFC 6265 section 5.2:
+                // ...
+                // 3.  If the remaining unparsed-attributes contains a %x3B (";")
+                //     character:
+                // Consume the characters of the unparsed-attributes up to,
+                // not including, the first %x3B (";") character.
+                // ...
+                stringifiedAttributes += '=' + attributes[attributeName].split(';')[0];
+            }
+
+            return (document.cookie = key + '=' + value + stringifiedAttributes);
+        }
+
+        function get(key, json) {
+            if (typeof document === 'undefined') {
+                return;
+            }
+
+            var jar = {};
+            // To prevent the for loop in the first place assign an empty array
+            // in case there are no cookies at all.
+            var cookies = document.cookie ? document.cookie.split('; ') : [];
+            var i = 0;
+
+            for (; i < cookies.length; i++) {
+                var parts = cookies[i].split('=');
+                var cookie = parts.slice(1).join('=');
+
+                if (!json && cookie.charAt(0) === '"') {
+                    cookie = cookie.slice(1, -1);
+                }
+
+                try {
+                    var name = decode(parts[0]);
+                    cookie = (converter.read || converter)(cookie, name) ||
+                        decode(cookie);
+
+                    if (json) {
+                        try {
+                            cookie = JSON.parse(cookie);
+                        } catch (e) {}
+                    }
+
+                    jar[name] = cookie;
+
+                    if (key === name) {
+                        break;
+                    }
+                } catch (e) {}
+            }
+
+            return key ? jar[key] : jar;
+        }
+
+        api.set = set;
+        api.get = function (key) {
+            return get(key, false /* read as raw */ );
+        };
+        api.getJSON = function (key) {
+            return get(key, true /* read as json */ );
+        };
+        api.remove = function (key, attributes) {
+            set(key, '', extend(attributes, {
+                expires: -1
+            }));
+        };
+
+        api.defaults = {};
+
+        api.withConverter = init;
+
+        return api;
+    }
+
+    return init(function () {});
+}));
+
+
+(function () {
+    "use strict";
+
+    var cookieAlert = document.querySelector(".cookiealert");
+    var acceptCookies = document.querySelector(".acceptcookies");
+
+    if (!cookieAlert) {
+        return;
+    }
+
+    createBanner();
+    
+    cookieAlert.offsetHeight;
+    // Show the alert if we cant find the "acceptCookies" cookie
+    if (Cookies.get("acceptCookies") == undefined || Cookies.get('acceptCookies') != "true") {
+        cookieAlert.classList.add("show");
+    }
+
+    // When clicking on the agree button, create a 1 year
+    // cookie to remember user's choice and close the banner
+    acceptCookies.addEventListener("click", function () {
+        setCookie("acceptCookies", true, 365);
+        Cookies.set("acceptCookies", true, {
+            expires: 365
+        });
+        cookieAlert.classList.remove("show");
+
+        // dispatch the accept event
+        window.dispatchEvent(new Event("cookieAlertAccept"))
+    });
+})();
+
+
+function createBanner(msg = '<b>Do you like cookies?</b>', btn_accept = "Accept", btn_config = "Configuration", url_config = "") {
+    cookie = `
+    <div class="alert text-center cookiealert" role="alert">
+		${msg}
+		
+        <button type="button" class="btn btn-yellow btn-sm acceptcookies"> ${btn_accept} </button>
+        <a href="${url_config}" type="button" class="btn btn-yellow btn-sm"> ${btn_config} </a>
+    </div>`;
+
+    document.body.innerHTML += cookie;
+}
